c856d30077269a67bb6a65111c649b5e6ca4459c
Blue Jo <blueajo@gmail.com>
2017-09-07 18:08:10 -0700

    fixed some resizing issues

--- index.js
+++ index.js
@@ -16,37 +16,36 @@ $(document).ready(function() {
   var box = document.getElementById('fadeup');
 
   var handleScroll = function () {
-    var t = 100 * body.scrollTop / (body.scrollHeight - window.innerHeight);
-    if(t >= 100) t = 99.99;
-    if(t <= 0) t = 0;
+    var t = body.scrollTop;
 
     var color1;
     var color2;
-    var top = false;
 
-    var textVisibleDelay = -(body.scrollTop / 700);
-    console.log(textVisibleDelay);
+    var textVisibleDelay = -(t / 700);
     if(textVisibleDelay < -.99) {
       textVisibleDelay = -.99;
     }
     document.getElementById('text').style.animationDelay = textVisibleDelay + 's';
 
-    if (t < 25) {
+    if (t < $('#About').offset().top) {
       color1 = colorMap.get('Blue');
       color2 = colorMap.get('About');
-      top = true;
-    } else if (t < 50) {
+      t = t / $('#About').offset().top;
+    } else if (t < $('#Coursework').offset().top) {
       color1 = colorMap.get('About');
       color2 = colorMap.get('Coursework');
-    } else if (t < 75) {
+      t = (t - $('#About').offset().top) / ($('#Coursework').offset().top - $('#About').offset().top);
+    } else if (t < $('#Skills').offset().top) {
       color1 = colorMap.get('Coursework');
       color2 = colorMap.get('Skills');
+      t = (t - $('#Coursework').offset().top) / ($('#Skills').offset().top - $('#Coursework').offset().top);
     } else {
       color1 = colorMap.get('Skills');
       color2 = colorMap.get('Experience');
+      t = (t - $('#Skills').offset().top) / ($('#Experience').offset().top - $('#Skills').offset().top);
     }
 
-    t = 100 - (t % 25) * 8;
+    t = 100 - 200 * t;
 
     var background;
 
@@ -79,3 +78,26 @@ $('a[href^="#"]').on('click', function(event) {
     }
 
 });
+
+function next() {
+
+  var t = window.document.body.scrollTop;
+  var target;
+
+  if (t < $('#About').offset().top - 1) {
+    target = "#About";
+  } else if (t < $('#Coursework').offset().top - 1) {
+    target = "#Coursework";
+  } else if (t < $('#Skills').offset().top - 1) {
+    target = "#Skills";
+  } else {
+    target = "#Experience";
+  }
+
+  if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $(target).offset().top,
+      }, 300);
+  }
+}
