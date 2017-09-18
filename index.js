e520d2a19be14d88df25a8b5bc2cdc5cb59d229f
Blue Jo <blueajo@gmail.com>
2017-09-17 22:26:58 -0700

    third attempt

--- index.js
+++ index.js
@@ -28,64 +28,23 @@ $(window).on('load', function() {
   }, 1600);
 });
 
+var supportsTextClip = CSS.supports("(background-clip: text) or (-webkit-background-clip: text)");
+var doesntSupportSticky = !CSS.supports("(position: sticky) or (position: -webkit-sticky)");
+
 // handles scroll-based animations
 (function () {
-
   var handleScroll = function () {
     var t = document.documentElement.scrollTop || document.body.scrollTop;
 
-
-    if (t >= $('#headerPosition').offset().top) {
-      // stick the div
-      document.getElementById('header').classList.add("stick");
-    } else {
-      // release the div
-      document.getElementById('header').classList.remove("stick");
+    if(doesntSupportSticky) {
+      stickyTop(t);
     }
 
-    var color1;
-    var color2;
+    bodyAppear(t);
 
-    var bodyOpacity = (t / 400);
-    if(bodyOpacity >= 1) {
-      bodyOpacity = 1;
+    if(supportsTextClip) {
+      colorChange(t);
     }
-    if(bodyOpacity <= 0) {
-      bodyOpacity = 0;
-    }
-
-    document.getElementById('body').style.opacity = bodyOpacity;
-
-    if (t < $('#About').offset().top) {
-      color1 = colorMap.get('Blue');
-      color2 = colorMap.get('About');
-      t = t / $('#About').offset().top;
-    } else if (t < $('#Education').offset().top) {
-      color1 = colorMap.get('About');
-      color2 = colorMap.get('Education');
-      t = (t - $('#About').offset().top) / ($('#Education').offset().top - $('#About').offset().top);
-    } else if (t < $('#Skills').offset().top) {
-      color1 = colorMap.get('Education');
-      color2 = colorMap.get('Skills');
-      t = (t - $('#Education').offset().top) / ($('#Skills').offset().top - $('#Education').offset().top);
-    } else {
-      color1 = colorMap.get('Skills');
-      color2 = colorMap.get('Projects');
-      t = (t - $('#Skills').offset().top) / ($('#Projects').offset().top - $('#Skills').offset().top);
-    }
-
-    t = 100 - 200 * t;
-
-    var background;
-
-    if (t > 0) {
-      background = "linear-gradient(" + color1 + " " + t + "%, " + color2 + ")";
-    } else {
-      background = "linear-gradient(" + color1 + " 0%, " + color2 + " " + (100 + t) + "%)";
-    }
-
-    document.getElementById("blue").style.backgroundImage = background + ', url("./images/intro_water.gif")';
-    document.getElementById("jo").style.backgroundImage = background;
   };
 
   $(document).ready(function() {
@@ -94,6 +53,68 @@ $(window).on('load', function() {
     }, 2040);
   });
 }());
+
+// handles sticky top for stupid browsers that don't support sticky (i.e. MICROSOFT)
+function stickyTop(t) {
+  if (t >= $('#headerPosition').offset().top) {
+    // stick the div
+    document.getElementById('header').classList.add("stick");
+  } else {
+    // release the div
+    document.getElementById('header').classList.remove("stick");
+  }
+}
+
+//handles appearance of body on scrollTopvar bodyOpacity = (t / 400);
+function bodyAppear(t) {
+  var bodyOpacity = (t / 400);
+
+  if(bodyOpacity >= 1) {
+    bodyOpacity = 1;
+  }
+  if(bodyOpacity <= 0) {
+    bodyOpacity = 0;
+  }
+
+  document.getElementById('body').style.opacity = bodyOpacity;
+}
+
+// handles color change animation for browers that support it (i.e. NOT MICROSOFT)
+function colorChange(t) {
+  var color1;
+  var color2;
+
+  if (t < $('#About').offset().top) {
+    color1 = colorMap.get('Blue');
+    color2 = colorMap.get('About');
+    t = t / $('#About').offset().top;
+  } else if (t < $('#Education').offset().top) {
+    color1 = colorMap.get('About');
+    color2 = colorMap.get('Education');
+    t = (t - $('#About').offset().top) / ($('#Education').offset().top - $('#About').offset().top);
+  } else if (t < $('#Skills').offset().top) {
+    color1 = colorMap.get('Education');
+    color2 = colorMap.get('Skills');
+    t = (t - $('#Education').offset().top) / ($('#Skills').offset().top - $('#Education').offset().top);
+  } else {
+    color1 = colorMap.get('Skills');
+    color2 = colorMap.get('Projects');
+    t = (t - $('#Skills').offset().top) / ($('#Projects').offset().top - $('#Skills').offset().top);
+  }
+
+  t = 100 - 200 * t;
+
+  var background;
+
+  if (t > 0) {
+    background = "linear-gradient(" + color1 + " " + t + "%, " + color2 + ")";
+  } else {
+    background = "linear-gradient(" + color1 + " 0%, " + color2 + " " + (100 + t) + "%)";
+  }
+
+  document.getElementById("blue").style.backgroundImage = background + ', url("./images/intro_water.gif")';
+  document.getElementById("jo").style.backgroundImage = background;
+}
 
 // handles smooth scrolling when clicking on # links
 $('a[href^="#"]').on('click', function(event) {
