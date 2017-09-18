7be6f79c24c5565ba295cc0f513e9b1edf63e4cf
Blue Jo <blueajo@gmail.com>
2017-09-17 23:18:47 -0700

    :(

--- index.js
+++ index.js
@@ -23,8 +23,36 @@ $(window).on('load', function() {
   }, 1600);
 });
 
-var supportsTextClip = CSS.supports("(background-clip: text) or (-webkit-background-clip: text)");
-var doesntSupportSticky = !CSS.supports("(position: sticky) or (position: -webkit-sticky)");
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return true;
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    return true;
+  }
+
+  // other browser
+  return false;
+}
+
+if(detectIE) {
+  var supportsTextClip = false;
+  var doesntSupportSticky = true;
+} else {
+  var supportsTextClip = CSS.supports("(background-clip: text) or (-webkit-background-clip: text)");
+  var doesntSupportSticky = !CSS.supports("(position: sticky) or (position: -webkit-sticky)");
+}
 
 // handles scroll-based animations
 (function () {
